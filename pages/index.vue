31ca8556e87f72d57b0d1cf36f92e9c1b5e8802e
Alfredo Hernández-R <alfredo.hernandez@ara-tech.es>
2024-05-09 19:52:54 +0200

    cambio a nuxt3

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
         <p class="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+          id.
         </p>
         <button class="btn btn-primary">Iniciar</button>
       </div>
