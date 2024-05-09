31575fc4511a4cf7c6bd594ca321507b973368c6
Alfredo Hernández-R <alfre2000bui@gmail.com>
2024-05-09 18:34:50 +0200

    cambio a nuxt3

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,33 @@
 <template>
-  <Tutorial/>
+  <div class="display flex flex-row-reverse">
+    <div class="dropdown mb-1">
+    <div tabindex="0" role="button" class="btn m-1">
+      Tema
+      <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+    </div>
+    <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Sistema" value="default"/></li>
+      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Oscuro" value="dark"/></li>
+      <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Claro" value="corporate"/></li>
+    </ul>
+    </div>
+  </div>
+  
+  <div class="hero bg-base-200 py-10">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">InDev Manager</h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <button class="btn btn-primary">Iniciar</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-import Vue from 'vue'
+<script setup>
 
 </script>
